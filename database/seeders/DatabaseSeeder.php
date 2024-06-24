dcd8cb14c79fd195e2e08bf06e5cb76ad37370f3
stevenchan7 <0stevenchan0@gmail.com>
2024-06-24 11:18:28 +0800

    add roles table and relation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            EmployeeSeeder::class
+            EmployeeSeeder::class,
+            RoleSeeder::class
         ]);
     }
 }
